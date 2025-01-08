f0fed14ac6bc074cd7c96f23d188ffab325eebcd
Don Bosco <don965877@gmail.com>
2025-01-08 08:59:14 +0300

    created new directory and cp files

---
+++ alx-project-0x02/alx-project-2/components/layout/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import home from '@/pages/home'
+import about from '@/pages/about'
+
+function Header() {
+  return (
+    <div>
+        <nav>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">About</a></li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default Header
