af5a0db0afc6acd32980615483bb91042b309698
Don Bosco <don965877@gmail.com>
2025-01-09 03:40:16 +0300

    refined Card component

--- alx-project-0x02/components/common/Card.tsx
+++ alx-project-0x02/components/common/Card.tsx
@@ -1,5 +1,4 @@
-import { CardProps } from "@/interfaces"
-
+import { type CardProps } from "@/interfaces";
 export const Card = ({ title, content }: CardProps) => (
     <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
         <h3 className="text-lg font-bold">{title}</h3>
