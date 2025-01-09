acbaccf80bfcb7bcddc0c1f3dcd99bcfe7600112
Don Bosco <don965877@gmail.com>
2025-01-09 06:02:16 +0300

    rectified button

--- alx-project-0x02/components/common/Button.tsx
+++ alx-project-0x02/components/common/Button.tsx
@@ -1,5 +1,9 @@
-import { ButtonProps } from "@/interfaces";
+import { type ButtonProps } from "@/interfaces";
 
 export const Button = ({ size, shape }: ButtonProps) => {
-    
+    return(
+        <button style={{ width: size, height: size, borderRadius: shape }}></button>
+    )
 }
+
+export default Button
