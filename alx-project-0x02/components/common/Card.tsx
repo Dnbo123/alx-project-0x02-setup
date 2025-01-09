02bec10e205db303d0836c2a07aa69b65b4ab0de
Don Bosco <don965877@gmail.com>
2025-01-09 03:36:30 +0300

    created card component

---
+++ alx-project-0x02/components/common/Card.tsx
@@ -0,0 +1,8 @@
+import { CardProps } from "@/interfaces"
+
+export const Card = ({ title, content }: CardProps) => (
+    <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <h3 className="text-lg font-bold">{title}</h3>
+        <p>{content}</p>
+    </div>
+);
--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Card } from '@/components/common/Card'
 
-export function home() {
-  return (
+const Home: React.FC = () => {
+  return(
     <div>
-        <h1>Home</h1>
+      <Card title="Home" content="Welcome to the project" />
+      <Card title="About Us" content="Learn More about our Company" />
+      <Card title="Contact Us" content="Get in touch with us" />
     </div>
   )
 }
 
-export default home
+export default Home;
